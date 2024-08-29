239b31a11ef9cad9670e7b671bf9fb69bd428e71
Santosh Kumar Sharma <108514898+santoshsharma143@users.noreply.github.com>
2024-08-29 15:12:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true }); //This line is to use input method in node.js
 
-let a = parseInt(prompt("Enter your first number : "))
+let a = parseInt(prompt("Enter your first number : ")) //Taking input form user 
 let b = prompt("Enter your operator number : ")
 let c = parseInt(prompt("Enter your second number : "))
-r = Math.random()
-if (r > 0.25 == true) {
+
+r = Math.random()  //Creating a random number for making 25% wrong operation
+
+//The random number generate number between 0 to 1 therefore we check the random number with 0.25
+if (r > 0.25 == true) { 
     if (b == "+") {
 
         console.log(a + c);
